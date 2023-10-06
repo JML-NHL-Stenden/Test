6f8f7c15fe520a2e060e2297046686df340bb8f0
Jia Men Lam <jia.men.lam@student.nhlstenden.com>
2023-10-06 14:46:16 +0200

    First commit, added testfile test.php

---
+++ test.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+echo "<p>Contain a string</p>";
+print ("<p>Contain something else</p>");
+
+
+// variables
+$fName = "Gerjan";
+$age = 32;
+$married = TRUE;
+$kids =2;
+$balance = 0.00;
+
+echo $fName,"<br";
+echo $age;
+
+
+echo "My age is ",$age+1;
+
+// Operator
+//  + - = / * % 
+
+       // age verification for the good stuff
+
+    
+
+
+
+?>
+
+
+    
+</body>
+</html>
+
+
+<!-- bvkadbfvkadfb -->
